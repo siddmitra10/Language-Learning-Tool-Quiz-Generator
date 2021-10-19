f4146639fb4fba7a64e83f6b28e265c2617488d7
siddmitra10 <67185810+siddmitra10@users.noreply.github.com>
2021-10-19 14:53:51 -0400

    Update quizQuestion.py

--- quizQuestion.py
+++ quizQuestion.py
@@ -1,23 +1,8 @@
 '''
 ===============================================================================
-ENGR 133 Program Description 
+Program Description 
     Quizzes the user on one specific English word.
-    
-Assignment Information
-	Assignment:     Individual Project
-	Author:         Siddharth Mitra, mitra30
-	Team ID:        002-17
-	
-Contributor:    Name, login@purdue [repeat for each]
-	My contributor(s) helped me:	
-	[ ] understand the assignment expectations without
-		telling me how they will approach it.
-	[ ] understand different ways to think about a solution
-		without helping me plan my solution.
-	[ ] think through the meaning of a specific error or
-		bug present in my code without looking at my code.
-	Note that if you helped somebody else with their code, you
-	have to list that person as a contributor here as well.
+Author:         Siddharth Mitra, mitra30
 ===============================================================================
 '''
 def quizQuestion(engWord,freWords):
@@ -55,12 +40,3 @@ def quizQuestion(engWord,freWords):
       print ("Correct!")
       print("-------------------------------------------------------")
       return not incorrect
-'''
-===============================================================================
-ACADEMIC INTEGRITY STATEMENT
-    I have not used source code obtained from any other unauthorized
-    source, either modified or unmodified. Neither have I provided
-    access to my code to another. The project I am submitting
-    is my own original work.
-===============================================================================
-'''
